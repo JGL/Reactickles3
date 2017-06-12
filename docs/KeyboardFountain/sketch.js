0ed4497d4d6aa6e489a1e2f464d4d98c6c8197f8
Joel Gethin Lewis <me@joelgethinlewis.com>
2017-06-12 20:02:57 +0100

    New version of KeyboardFountain and first version of full suite

--- docs/KeyboardFountain/sketch.js
+++ docs/KeyboardFountain/sketch.js
@@ -4,7 +4,7 @@ var randomColour;
 //matter-js and p5.js integration based on https://github.com/shiffman/p5-matter by Daniel Shiffman
 //also see https://www.youtube.com/watch?v=urR596FsU68 introduction to matter.js by Daniel Shiffman
 var Engine = Matter.Engine;
-//var Render = Matter.Render; // commented out as we are using p5.js to render everything to the screen
+var Render = Matter.Render; // commented out as we are using p5.js to render everything to the screen
 var World = Matter.World;
 var Bodies = Matter.Bodies;
 var Body = Matter.Body;
@@ -18,15 +18,16 @@ var canvas;
 
 var circles = [];//array holding the circles in the simulation
 
-var fountainWidth = 50; //50 pixels wide
-var fountainHeight = 50; //50 pixels high
+var fountainWidth = 10;
+var fountainHeight = 50;
+
+var fountain;
 
 function setup() {
   canvas = createCanvas(windowWidth,windowHeight); //make a fullscreen canvas, thanks to: http://codepen.io/grayfuse/pen/wKqLGL
   noStroke(); //no outlines, just filled shapes
   colorMode(HSB, 100);// Use HSB with scale of 0-100, see https://p5js.org/reference/#/p5/color
   rectMode(CENTER); // draw rects from their centres... https://p5js.org/reference/#/p5/rectMode
-  pickRandomColour();
   fountainPosition = createVector(0.5,1); //half way across the screen, at the bottom of the screen
 
   setupPhysics();
@@ -41,7 +42,7 @@ function setupPhysics(){
   var params = {
     isStatic: true
   }
-  var ground = Bodies.rectangle(width / 2, height, width, 1, params);
+  var ground = Bodies.rectangle(width / 2, height+1, width, 1, params); //+1 so it's just below the bottom of the screen, Matter.Bodies.rectangle(x, y, width, height, [options])
   var leftWall = Bodies.rectangle(0, height / 2, 1, height, params);
   var rightWall = Bodies.rectangle(width, height / 2, 1, height, params);
   var top = Bodies.rectangle(width / 2, 0, width, 1, params);
@@ -49,6 +50,12 @@ function setupPhysics(){
   World.add(world, leftWall);
   World.add(world, rightWall);
   World.add(world, top);
+
+  //adding the fountain
+  var fountainX = fountainPosition.x*windowWidth;
+  var fountainY = fountainPosition.y*windowHeight; //to get it above the ground! otherwise it would fall forever!
+  fountain = Bodies.rectangle(fountainX-(fountainWidth/2), fountainY-(fountainHeight/2), fountainWidth, fountainHeight, params); //position is in the centre of it
+  World.add(world, fountain);
 
   // run the engine
   Engine.run(engine);
@@ -69,18 +76,27 @@ function moveFountain(){
 
   if (keyIsDown(RIGHT_ARROW) && (this.fountainPosition.x <= (1-moveAmount)))
     this.fountainPosition.x += moveAmount;
-}
 
-function pickRandomColour(){
-  randomColour = color(random(100),50,100,50); //random hue, saturation 50%, brightness 100%, alpha 50%
+  var positionVector = Matter.Vector.create((this.fountainPosition.x*windowWidth)-(fountainWidth/2), (this.fountainPosition.y*windowHeight)-(fountainHeight/2));
+  Body.setPosition(fountain, positionVector);
 }
 
 function drawFountain(){
   var translatedX = this.fountainPosition.x * windowWidth;
   var translatedY = this.fountainPosition.y * windowHeight;
-  var redColour = color(0,100,100,100);
+  //var redColour = color(0,100,100,100);
+  var redColour = color('rgba(0,0,0,0.5)');
+  var blueColour = color('rgba(0,0,0,0.5)');
+
   fill(redColour);
-  triangle(translatedX-(fountainWidth/2),translatedY,translatedX+(fountainWidth/2),translatedY,translatedX,translatedY-fountainHeight); //https://p5js.org/reference/#/p5/triangle
+  rect(translatedX-(fountainWidth/2), translatedY-(fountainHeight/2), fountainWidth, fountainHeight);//rect(x,y,w,h
+  //triangle(translatedX-(fountainWidth/2),translatedY,translatedX+(fountainWidth/2),translatedY,translatedX,translatedY-fountainHeight); //https://p5js.org/reference/#/p5/triangle
+
+  //also draw the simulated triangle
+  var posFountainX = this.fountain.position.x;
+  var posFountainY = this.fountain.position.y;
+  fill(blueColour);
+  rect(posFountainX, posFountainY, fountainWidth, fountainHeight);
 }
 
 function addCircle(){
@@ -92,7 +108,8 @@ function addCircle(){
   var translatedY = this.fountainPosition.y * windowHeight;
   var radius = 21;
   var newCircle = Bodies.circle(translatedX, translatedY-(fountainHeight), radius, params);
-  circles.push(newCircle);
+  var theCircleToAdd = new FountainCircle(newCircle);
+  circles.push(theCircleToAdd);
   World.add(world, newCircle);
 
   //set a random velocity of the new circle
@@ -107,18 +124,18 @@ function addCircle(){
 function drawCircles(){
   stroke(255);
   strokeWeight(1);
-  fill(randomColour);
 
   for (var i = 0; i < circles.length; i++) {
-    var circle = circles[i];
+    var circle = circles[i].matterCircle;
     var pos = circle.position;
     var r = circle.circleRadius;
     var angle = circle.angle;
     push();
     translate(pos.x, pos.y);
     rotate(angle);
+    fill(circles[i].colour);
     ellipse(0, 0, r * 2);
-    line(0, 0, r, 0);
+    //line(0, 0, r, 0);
     pop();
   }
 }
@@ -130,4 +147,9 @@ function keyTyped(){
 
 function windowResized() { //https://p5js.org/reference/#/p5/windowResized
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function FountainCircle(theCircle) {  // quick class to hold Matter Circle and its colour
+  this.matterCircle = theCircle;
+  this.colour = color(random(100),50,100,50); //random hue, saturation 50%, brightness 100%, alpha 50%;
 }
---
+++ docs/Reactickles3/KeyboardSpringyCircles.js
@@ -0,0 +1,104 @@
+function KeyboardSpringyCircles(){
+  var springyCircles = []; //array of SpringyCircle objects
+  var numberOfSpringyCircles = 100;
+  var allTheKeys = "1234567890qwertyuiopasdfghjklzxcvbnm";
+  var circleMinRadius = 50;
+  var circleMaxRadius = 100;
+
+  this.setup = function(){
+    createCanvas(windowWidth,windowHeight); //make a fullscreen canvas, thanks to: http://codepen.io/grayfuse/pen/wKqLGL
+    noStroke(); //no outlines, just filled shapes
+    colorMode(HSB, 100);// Use HSB with scale of 0-100, see https://p5js.org/reference/#/p5/color
+    ellipseMode(RADIUS); //https://p5js.org/reference/#/p5/ellipseMode draw with a radius rather than a width
+
+    for (var i=0; i < numberOfSpringyCircles; i++) {
+      springyCircles.push(new SpringyCircle());
+    }
+  }
+
+  this.draw = function(){
+    background(255); //white background
+    this.drawCircles();
+  }
+
+  this.drawCircles = function(){
+    for (var i = 0; i < springyCircles.length; i++) {
+      springyCircles[i].display();
+    }
+  }
+
+  this.keyTyped = function(){
+    var lowerCaseKey = key.toLowerCase(); //key is a system variable via https://p5js.org/reference/#/p5/key, toLowerCase via http://www.w3schools.com/jsref/jsref_tolowercase.asp
+
+    if(allTheKeys.includes(lowerCaseKey)){
+      //if the key is a valid one, circles nearby should react
+      var positionOfKey = getCanvasPositionFromKey(lowerCaseKey);
+      var positionOfKeyInPixels = createVector(positionOfKey.x * windowWidth, positionOfKey.y * windowHeight);
+
+      for (var i = 0; i < springyCircles.length; i++) {
+        //for all the springyCircles
+        var positionOfCircleInPixels = createVector(springyCircles[i].position.x * windowWidth, springyCircles[i].position.y * windowHeight);
+        var radiusOfCircle = springyCircles[i].radius;
+        var distanceBetweenKeyAndCircle = dist(positionOfKeyInPixels.x, positionOfKeyInPixels.y, positionOfCircleInPixels.x, positionOfCircleInPixels.y);
+
+        if(distanceBetweenKeyAndCircle < radiusOfCircle){
+          //if the key is under the springy circle, then spring/move it
+          springyCircles[i].moveCircle();
+        }
+
+      }
+    }
+
+    return false; //https://p5js.org/reference/#/p5/keyTyped preventing default behaviour
+  }
+
+  this.mouseReleased = function(){
+    return false; //https://p5js.org/reference/#/p5/mouseReleased preventing default behaviour
+  }
+
+  this.touchMoved = function(){
+    return false; //https://p5js.org/reference/#/p5/touchMoved
+  }
+
+  this.windowResized = function(){//https://p5js.org/reference/#/p5/windowResized
+    resizeCanvas(windowWidth, windowHeight);
+  }
+
+  function SpringyCircle(){ //SpringyCircle object
+    //converted from Processing.js http://processingjs.org/learning/topic/springs/
+    this.colour = color(random(100),50,100,50);; //random hue, saturation 50%, brightness 100%, alpha 50%
+    this.radius = random(circleMinRadius,circleMaxRadius);
+    this.mass = this.radius/10.0;
+    this.springConstant = 0.5; //aka "k" in Hookes law https://en.wikipedia.org/wiki/Hooke's_law
+    this.damping = 0.9;
+    this.restPosition = createVector(random(windowWidth)/windowWidth,random(windowHeight)/windowHeight);
+    this.position = createVector(this.restPosition.x, this.restPosition.y);
+    this.velocity = createVector(0,0);
+    this.acceleration = createVector(0,0);
+    this.force = createVector(0,0);
+
+    this.display = function(){
+      // can't do these things below, need to call dedicated static p5.vector methods, see: https://p5js.org/reference/#/p5.Vector
+      // this.force = -this.springConstant * (this.position-this.restPosition); // f=-ky
+      // this.acceleration = this.force/this.mass; // Set the acceleration, f=ma == a=f/m
+      // this.velocity = this.damping * (this.velocity+this.acceleration);
+      // this.position += this.velocity;
+      this.force = p5.Vector.mult(p5.Vector.sub(this.position,this.restPosition),-this.springConstant); // f=-ky
+      this.acceleration = p5.Vector.div(this.force,this.mass); // Set the acceleration, f=ma == a=f/m
+      this.velocity = p5.Vector.mult(p5.Vector.add(this.velocity, this.acceleration),this.damping);
+      this.position.add(this.velocity);
+
+      var translatedX = this.position.x * windowWidth;
+      var translatedY = this.position.y * windowHeight;
+      fill(this.colour);
+      ellipse(translatedX, translatedY, this.radius); // https://p5js.org/reference/#/p5/ellipse and https://p5js.org/reference/#/p5/ellipseMode
+    }
+
+    this.moveCircle = function(){ //move the position of the spring a bit
+      var randomOffset = createVector(random(-0.1,0.1), random(-0.1,0.1));
+      //this.position += randomOffset; += doesn't work!
+      this.position.add(randomOffset); //see https://p5js.org/examples/hello-p5-flocking.html for more p5.vector fun
+      // and http://p5js.org/reference/#/p5.Vector
+    }
+  }
+}
